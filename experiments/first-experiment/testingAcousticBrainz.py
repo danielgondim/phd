3e4d8ce225340486998ed3a7780a909572491076
Daniel Gondim <danielgondimm@gmail.com>
2017-02-13 23:40:37 -0300

    Retrieving musics of a certain period of time

--- experiments/first-experiment/testingAcousticBrainz.py
+++ experiments/first-experiment/testingAcousticBrainz.py
@@ -1,29 +1,68 @@
-import requests, sys, acoustic_brainz, music_brainz, charts, time
+import requests, sys, acoustic_brainz, music_brainz, time, datetime, calendar
 
 from unidecode import unidecode
 
 API_KEY = sys.argv[1]
-LASTFM_USERNAME = "danielgem" 
-LIMIT_OF_MUSICS = "30"
+LASTFM_USERNAME = "felipevf" 
+LIMIT_OF_MUSICS = "10"
+TIMESTAMP_DIFFERENCE = 14400
 INITIAL_TIME = str(int(time.time()) - 3600)
 FINAL_TIME = str(int(time.time()))
 API_REST_LASTFM = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + \
 	LASTFM_USERNAME + "&api_key=" + API_KEY + "&format=json&limit=" + LIMIT_OF_MUSICS
-#API_REST_LASTFM = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + \
-	#LASTFM_USERNAME + "&api_key=" + API_KEY + "&format=json&limit=" + LIMIT_OF_MUSICS + "&from=" + \
-	#INITIAL_TIME + "&to=" + FINAL_TIME
+API_REST_LASTFM_TIME = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + \
+	LASTFM_USERNAME + "&api_key=" + API_KEY + "&format=json&from=" + INITIAL_TIME + "&to=" + FINAL_TIME
 
-def sum_high_level_features(current_features):
-	global music_features
-	for feature in music_features.keys():
-		music_features[feature] = music_features[feature] + current_features[feature]
+def is_weekend(date):
+	return date.weekday() >= 5 
 
+def convert_to_datetime(date):
+	return datetime.datetime.strptime(date + " 00:00:00", "%Y-%m-%d %H:%M:%S")
 
+def api_rest_with_timestamp(date_time):
+	timestamp = calendar.timegm(date_time.utctimetuple())
+	return "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + \
+	LASTFM_USERNAME + "&api_key=" + API_KEY + "&format=json&from=" + str(timestamp) + "&to=" + \
+	str(timestamp + TIMESTAMP_DIFFERENCE)
+
+current_date = convert_to_datetime(sys.argv[2])
+current_date += datetime.timedelta(hours=11)
+final_date = convert_to_datetime(sys.argv[3])
+final_date += datetime.timedelta(hours=11)
+
+while (current_date <= final_date):
+	if (current_date.weekday() < 5):
+		#pegar musicas pela manha
+		print "Musicas da manha do dia: " + str(current_date)
+		print api_rest_with_timestamp(current_date)
+		response = requests.get(api_rest_with_timestamp(current_date))
+		data = response.json()
+		print data
+
+		print "################################"
+
+		#musicas da tarde
+		current_date += datetime.timedelta(hours=6)
+		print api_rest_with_timestamp(current_date)
+		print "Musicas da tarde do dia: " + str(current_date)
+		response = requests.get(api_rest_with_timestamp(current_date))
+		data = response.json()
+		print data
+
+		print "################################"
+
+		#passando para o inicio do expediente do outro dia
+		current_date += datetime.timedelta(hours=18)
+	else:
+		#se for fds passa o dia
+		print "Hoje eh fim de semana, nao teve trabalho!!!!"
+		print "################################"
+		current_date += datetime.timedelta(days=1)
+
+'''
 response = requests.get(API_REST_LASTFM)
 data = response.json()
 
-global music_features
-music_features = {"mood_electronic":0, "mood_party":0, "mood_aggressive":0, "mood_acoustic":0, "mood_happy":0, "mood_sad":0, "mood_relaxed":0}
 musicCount = 0
 for track in data['recenttracks']['track']:
 	MBIDs = []
@@ -49,7 +88,7 @@ for track in data['recenttracks']['track']:
 		else:
 			print "Low level features not retrieved"
 		#	sum_high_level_features(features)
+	else:
+		print "MBID not retrieved"
 	print "###########################################"
-
-#print music_features
-#charts.pie_chart(music_features)
+'''
