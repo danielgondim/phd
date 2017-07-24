87268ae970ea98bde66ffd649e58572d023e9b07
Daniel Gondim Ernesto de Melo <danielgondim@coddy.lsd.ufcg.edu.br>
2017-07-24 10:51:17 -0300

    cross validation

--- experiments/project-2017-1/logistic_regression_model.R
+++ experiments/project-2017-1/logistic_regression_model.R
@@ -9,7 +9,63 @@ summary(model)
 
 anova(model, test="Chisq")
 
+library(caret)
+
+
+acc <- NULL
+for(i in 1:nrow(focus_songs))
+{
+  # Train-test splitting
+  # 499 samples -> fitting
+  # 1 sample -> testing
+  train <- focus_songs[-i,]
+  test <- focus_songs[i,]
+  
+  # Fitting
+  model <- glm(focus ~ voice_instrumental_value+mood_acoustic_value+mood_relaxed_value,family=binomial,data=train)
+  
+  # Predict results
+  results_prob <- predict(model,newdata=test,type='response')
+  
+  # If prob > 0.5 then 1, else 0
+  results <- ifelse(results_prob > 0.5,1,0)
+  
+  # Actual answers
+  answers <- test$focus
+  
+  # Calculate accuracy
+  misClasificError <- mean(answers != results)
+  
+  # Collecting results
+  acc[i] <- 1-misClasificError
+}
+
+# Average accuracy of the model
+mean(acc)
+
+# Histogram of the model accuracy
+hist(acc,xlab='Accuracy',ylab='Freq',main='Accuracy LOOCV',
+     col='cyan',border='blue',density=30)
+
+################################
+
+
+
+
+
+# define training control
+train_control<- trainControl(method="cv", number=10)
+
+# train the model 
+model_cv<- train(focus ~ voice_instrumental_value+mood_acoustic_value+mood_relaxed_value,
+              data=focus_songs, trControl=train_control, method="glm", family=binomial(link='logit'))
+
+# print cv scores
+summary(model_cv)
+
 library(pscl)
+library(lattice)
+library(ggplot2)
 library(caret)
 
 pR2(model)
@@ -47,6 +103,13 @@ pR2(model3)
 fitted.results3 <- predict(model3,newdata=test,type='response')
 fitted.results3 <- ifelse(fitted.results3 > 0.5,1,0)
 misClasificError3 <- mean(fitted.results3 != test$focus)
+error <- sum(fitted.results3 != test$focus)
+error
+hits <- sum(fitted.results3 == test$focus)
+hits
+total <- nrow(test)
+total
+
 print(paste('Accuracy',1-misClasificError3))
 
 #com as variaveis independentes: voice_instrumental e acoustic
@@ -56,5 +119,5 @@ summary(model4)
 
 fitted.results4 <- predict(model4,newdata=test,type='response')
 fitted.results4 <- ifelse(fitted.results4 > 0.5,1,0)
-misClasificError4 <- mean(fitted.results4 != test$focus)
+misClasificError4 <- sum(fitted.results4 != test$focus)
 print(paste('Accuracy',1-misClasificError4))
